1e4e09c2a8d62b7619ce750dc0000b36b61094fc
Matheus Ribeiro <thesirion@gmail.com>
2024-02-06 20:26:37 -0300

    updated StoryCard to better adapt to different screen sizes

--- components/component/story-card.tsx
+++ components/component/story-card.tsx
@@ -6,6 +6,16 @@ import Link from 'next/link';
 const StoryCard = (props: { data: Post; variant?: string }) => {
   const { mainImage, title, excerpt, slug } = props.data;
 
+  let titleVariant = '';
+
+  if (props.variant === 'top') {
+    titleVariant = 'text-2xl lg:text-3xl';
+  } else if (props.variant === 'featured') {
+    titleVariant = 'text-2xl lg:text-xl';
+  } else {
+    titleVariant = 'text-xl';
+  }
+
   return (
     <Link
       href={`/blog/${slug}`}
@@ -14,17 +24,14 @@ const StoryCard = (props: { data: Post; variant?: string }) => {
         <Image
           alt={mainImage.alt}
           className={`w-full object-cover ${
-            props.variant === 'top' ? 'lg:h-100' : 'h-40'
+            props.variant === 'top' ? 'lg:h-96 xl:h-100' : 'h-40'
           }`}
           height='225'
           width='400'
           src={urlFor(mainImage.image).url()}
         />
       </div>
-      <h2
-        className={`mt-4 font-bold leading-tight ${
-          props.variant === 'top' ? 'text-2xl lg:text-3xl' : 'text-2xl'
-        }`}>
+      <h2 className={`mt-4 font-bold leading-tight ${titleVariant}`}>
         {title}
       </h2>
       <p className='mt-2 text-gray-500 text-sm line-clamp-2 prose'>{excerpt}</p>
