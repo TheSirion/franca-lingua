21dbfe7e4f3683fe5c15c3a74f401f3e36f91b88
Matheus Ribeiro <thesirion@gmail.com>
2024-02-10 23:59:49 -0300

    added metadata to Home and blog pages

--- app/(main)/blog/[slug]/page.tsx
+++ app/(main)/blog/[slug]/page.tsx
@@ -2,13 +2,27 @@ import { urlFor } from '@/app/lib/sanity/client';
 import { fetchPost } from '@/app/lib/sanity/fetch-data';
 import { Separator } from '@/components/ui/separator';
 import { PortableText } from '@portabletext/react';
-import Head from 'next/head';
 import Image from 'next/image';
 
 export const revalidate = 60;
 
+type Props = {
+  params: { slug: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export const generateMetadata = async ({ params, searchParams }: Props) => {
+  const post = await fetchPost(params.slug);
+  return {
+    title: `${post.title} | Franca Língua, o guia do estudante de línguas`,
+    description: post.excerpt,
+    image: urlFor(post.mainImage.image).url(),
+  };
+};
+
 const BlogPost = async ({ params }: { params: { slug: string } }) => {
   const post = await fetchPost(params.slug);
+
   const blogImageUrl = urlFor(post.mainImage.image).url();
   const authorImageUrl = urlFor(post.author.image).url();
 
@@ -24,107 +38,66 @@ const BlogPost = async ({ params }: { params: { slug: string } }) => {
   });
 
   return (
-    <>
-      <Head>
-        <title>{`Franca Língua | ${post.title}`}</title>
-        <meta
-          name='description'
-          content='O seu lugar para aprender a aprender línguas'
+    <div className='w-full md:w-3/4 lg:w-2/3 mx-auto my-8 px-5 flex flex-col lg:flex-row justify-center'>
+      <main className='w-full lg:w-3/4 space-y-8'>
+        <Image
+          alt={'Post title'}
+          className='w-full aspect-[2/1] object-cover rounded-lg'
+          height={400}
+          src={blogImageUrl}
+          width={800}
         />
-        <meta
-          property='og:title'
-          content={post.title}
-        />
-        <meta
-          property='og:description'
-          content={post.excerpt}
-        />
-        <meta
-          property='og:image'
-          content={blogImageUrl}
-        />
-        <meta
-          property='og:url'
-          content={`https://www.franca-lingua.com.br/blog/${post.slug}`}
-        />
-        <meta
-          name='twitter:card'
-          content='summary_large_image'
-        />
-        <meta
-          name='twitter:title'
-          content={post.title}
-        />
-        <meta
-          name='twitter:description'
-          content={post.excerpt}
-        />
-        <meta
-          name='twitter:image'
-          content={blogImageUrl}
-        />
-      </Head>
-      <div className='w-full md:w-3/4 lg:w-2/3 mx-auto my-8 px-5 flex flex-col lg:flex-row justify-center'>
-        <main className='w-full lg:w-3/4 space-y-8'>
-          <Image
-            alt={'Post title'}
-            className='w-full aspect-[2/1] object-cover rounded-lg'
-            height={400}
-            src={blogImageUrl}
-            width={800}
+        <h1 className='text-4xl font-bold'>{post.title}</h1>
+        <div className='flex flex-col md:flex-row'>
+          <div className='space-y-1'>
+            <p className='text-sm font-semibold'>Publicado em</p>
+            <time
+              dateTime={post.publishedAt}
+              className='text-sm text-gray-500'>
+              {createdAt}
+            </time>
+          </div>
+          <Separator
+            orientation='vertical'
+            className='mx-3 prose dark:prose-invert'
           />
-          <h1 className='text-4xl font-bold'>{post.title}</h1>
-          <div className='flex flex-col md:flex-row'>
-            <div className='space-y-1'>
-              <p className='text-sm font-semibold'>Publicado em</p>
-              <time
-                dateTime={post.publishedAt}
-                className='text-sm text-gray-500'>
-                {createdAt}
-              </time>
-            </div>
-            <Separator
-              orientation='vertical'
-              className='mx-3 prose dark:prose-invert'
-            />
-            <div className='space-y-1'>
-              <p className='text-sm font-semibold'>Atualizado em</p>
-              <time
-                dateTime={post._updatedAt}
-                className='text-sm text-gray-500'>
-                {updatedAt}
-              </time>
-            </div>
+          <div className='space-y-1'>
+            <p className='text-sm font-semibold'>Atualizado em</p>
+            <time
+              dateTime={post._updatedAt}
+              className='text-sm text-gray-500'>
+              {updatedAt}
+            </time>
           </div>
-          <p className='prose prose-lg font-light dark:prose-invert'>
-            {post.excerpt}
-          </p>
-          <Separator className='my-8' />
-          <article className='prose max-w-none dark:prose-invert'>
-            <PortableText value={post.body} />
-          </article>
-          <div className='flex flex-col items-center space-y-4'>
-            <Image
-              alt={`Escrito por ${post.author.name}`}
-              className='w-24 h-24 rounded-full object-cover'
-              height={100}
-              src={authorImageUrl}
-              style={{
-                aspectRatio: '100/100',
-                objectFit: 'cover',
-              }}
-              width={100}
-            />
-            <h3 className='text-lg font-semibold prose dark:prose-invert'>
-              {post.author.name}
-            </h3>
-            <div className='prose dark:prose-invert'>
-              <PortableText value={post.author.bio} />
-            </div>
+        </div>
+        <p className='prose prose-lg font-light dark:prose-invert'>
+          {post.excerpt}
+        </p>
+        <Separator className='my-8' />
+        <article className='prose max-w-none dark:prose-invert'>
+          <PortableText value={post.body} />
+        </article>
+        <div className='flex flex-col items-center space-y-4'>
+          <Image
+            alt={`Escrito por ${post.author.name}`}
+            className='w-24 h-24 rounded-full object-cover'
+            height={100}
+            src={authorImageUrl}
+            style={{
+              aspectRatio: '100/100',
+              objectFit: 'cover',
+            }}
+            width={100}
+          />
+          <h3 className='text-lg font-semibold prose dark:prose-invert'>
+            {post.author.name}
+          </h3>
+          <div className='prose dark:prose-invert'>
+            <PortableText value={post.author.bio} />
           </div>
-        </main>
-      </div>
-    </>
+        </div>
+      </main>
+    </div>
   );
 };
 
--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -4,14 +4,34 @@ import { Separator } from '@/components/ui/separator';
 
 export const revalidate = 60;
 
-export default function Home() {
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Franca Língua, o guia do estudante de línguas',
+  description:
+    'Dicas, tutoriais, curiosidades e tudo o que você precisa para aprender com eficiência',
+  keywords: [
+    'aprender',
+    'línguas',
+    'idiomas',
+    'linguística',
+    'dicas',
+    'tutoriais',
+    'curiosidades',
+    'línguas estrangeiras',
+    'inglês',
+    'espanhol',
+    'francês',
+    'alemão',
+  ],
+};
+
+export default async function Home() {
   return (
-    <main>
-      <div className='w-5/6 lg:w-2/3 m-auto md:px-6 lg:px-8 py-12'>
-        <FeaturedStories />
-        <Separator className='my-12' />
-        <StoryGrid />
-      </div>
+    <main className='w-5/6 lg:w-2/3 m-auto md:px-6 lg:px-8 py-12'>
+      <FeaturedStories />
+      <Separator className='my-12' />
+      <StoryGrid />
     </main>
   );
 }
