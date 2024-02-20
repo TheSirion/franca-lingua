c2757f2521688284fcd8afea470608306af2fafc
Matheus Ribeiro <thesirion@gmail.com>
2024-02-19 23:41:34 -0300

    centered image and text, made adjusted maximum image height to 750px

--- components/component/blog-image.tsx
+++ components/component/blog-image.tsx
@@ -5,17 +5,17 @@ import Image from 'next/image';
 const BlogImage = ({ value, isInline }: { value: any; isInline: any }) => {
   const { width, height } = getImageDimensions(value);
   return (
-    <>
+    <div className='flex flex-col justify-center items-center'>
       <Image
         src={urlFor(value).url()}
         alt={value.alt}
         width={width}
         height={height}
         loading='lazy'
-        className='rounded-lg mb-0'
+        className='rounded-lg mb-0 max-h-[750px] w-fit'
       />
       <p className='text-sm mt-2'>{value.alt}</p>
-    </>
+    </div>
   );
 };
 
